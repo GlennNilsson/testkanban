2e895629f8eb43e03373ed438cf3a104dbf14752
GlennNilsson <glenn_r_w_n@hotmail.com>
2020-10-11 23:37:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+import { CreateView, CreateKanBan} from './Modules/view.mjs';
+//Use this function for adding Header and footer
+CreateView();
+
+
+//Use function for creating boards
+CreateKanBan();
+
+
+
